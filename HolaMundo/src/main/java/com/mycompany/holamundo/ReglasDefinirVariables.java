6e51d774ee093649c78cae1a94eea27e19fd779f
Catalina Carreto <isccatalinacarreto27@gmail.com>
2019-10-14 13:22:49 -0500

    Reglas definir variables en Java

---
+++ HolaMundo/src/main/java/com/mycompany/holamundo/ReglasDefinirVariables.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author katyc
+ */
+public class ReglasDefinirVariables {
+    
+    public static void main(String arg[]){
+    
+       var saludasDesdeJava = "Hola desde Java ";
+        System.out.println(saludasDesdeJava);
+        
+        //Reglas para crear identificadores en Java
+        int _holaDesdeJava;
+        float $adios;
+        char adios;
+    }
+}
