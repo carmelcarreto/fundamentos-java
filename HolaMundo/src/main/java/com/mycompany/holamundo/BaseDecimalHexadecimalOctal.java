8d517d678b5242834f7ecd52dee031500c133680
Catalina Carreto <isccatalinacarreto27@gmail.com>
2019-10-14 18:44:12 -0500

    feat: Base Decimal, Hexadecimal, Octal y Binario

---
+++ HolaMundo/src/main/java/com/mycompany/holamundo/BaseDecimalHexadecimalOctal.java
@@ -0,0 +1,24 @@
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author katyc
+ */
+public class BaseDecimalHexadecimalOctal {
+
+    public static void main(String args[]) {
+
+        int numeroDecimal = 10;
+        System.out.println("numeroDecimal = " + numeroDecimal);
+
+        int numeroOctal = 012;
+        System.out.println("numeroOctal = " + numeroOctal);
+
+        int numeroHexadecimal = 0XA;
+        System.out.println("numeroHexadecimal = " + numeroHexadecimal);
+
+        int numeroBinario = 0b1010;
+        System.out.println("numeroBinario = " + numeroBinario);
+    }
+
+}
