88a85a3faa4d5772043a4d69f6492e5b4328557f
Catalina Carreto <isccatalinacarreto27@gmail.com>
2019-10-14 19:09:57 -0500

    feat: Tipos Primitivos tipo Char

---
+++ HolaMundo/src/main/java/com/mycompany/holamundo/TiposPrimitivosTipoChar.java
@@ -0,0 +1,27 @@
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author katyc
+ */
+public class TiposPrimitivosTipoChar {
+
+    public static void main(String args[]) {
+
+        char charVar = '\u0021';
+        System.out.println("charVar = " + charVar);
+
+        char varCharDecimal = 33;
+        System.out.println("varCharDecimal = " + varCharDecimal);
+        
+        char varCharSimbolo = '!';
+        System.out.println("varCharSimbolo = " + varCharSimbolo);
+
+        System.out.println("bits tipo char: " + Character.SIZE);
+        System.out.println("bytes tipo char: " + Character.BYTES);
+        System.out.println("valor minimo tipo char: " + Character.MIN_VALUE);
+        System.out.println("valor maximo tipo char: " + Character.MAX_VALUE);
+
+    }
+
+}
