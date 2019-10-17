a4865c2d5f9a00e49ef92d29b929687445259620
Catalina Carreto <isccatalinacarreto27@gmail.com>
2019-10-17 18:54:13 -0500

    feat: Ciclo Do While

---
+++ HolaMundo/src/main/java/com/mycompany/holamundo/CicloDoWhile.java
@@ -0,0 +1,17 @@
+package com.mycompany.holamundo;
+/**
+ *
+ * @author katyc
+ */
+public class CicloDoWhile {
+    
+    public static void main(String args[]) {
+
+        var contador = 0;
+        
+        do {
+            System.out.println("contador = " + contador);
+            contador++;
+        } while (contador < 3);
+    } 
+}
