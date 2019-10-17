b096706c9558eb3f10d1a1bfbae7dbdda06b36d5
Catalina Carreto <isccatalinacarreto27@gmail.com>
2019-10-17 18:52:38 -0500

    refactor: Identacion de codigo

--- HolaMundo/src/main/java/com/mycompany/holamundo/CicloWhile.java
+++ HolaMundo/src/main/java/com/mycompany/holamundo/CicloWhile.java
@@ -1,18 +1,16 @@
 package com.mycompany.holamundo;
-
 /**
  *
  * @author katyc
  */
 public class CicloWhile {
-
-    public static void main(String args[]) {
-
-        var contador = 0;
+    
+    public static void main (String args[]){
         
-        while (contador < 3) {
-           System.out.println("contador = " + contador);
-           contador++;
-        }  
+        var contador = 0;
+        while(contador < 3){
+            System.out.println("contador = " + contador);
+            contador ++;
+        }
     }
 }
