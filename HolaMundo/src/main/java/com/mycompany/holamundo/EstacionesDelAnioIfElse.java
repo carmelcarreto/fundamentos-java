cba8c8aece1a51fd7010de859967663d6501fb70
Catalina Carreto <isccatalinacarreto27@gmail.com>
2019-10-17 17:56:05 -0500

    refactor: Estaciones del año con sentencia If Else

--- HolaMundo/src/main/java/com/mycompany/holamundo/EstacionesDelAnioIfElse.java
+++ HolaMundo/src/main/java/com/mycompany/holamundo/EstacionesDelAnioIfElse.java
@@ -1,18 +1,11 @@
 package com.mycompany.holamundo;
 
-
 import java.util.Scanner;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author katyc
  */
-public class EstacionesDelAnio {
+public class EstacionesDelAnioIfElse {
 
     public static void main(String args[]) {
 
