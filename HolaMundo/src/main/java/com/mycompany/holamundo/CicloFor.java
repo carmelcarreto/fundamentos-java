d1613fab091a7261721bb6afcff53564d3a09677
Catalina Carreto <isccatalinacarreto27@gmail.com>
2019-10-18 11:17:33 -0500

    feat: Ciclo For

---
+++ HolaMundo/src/main/java/com/mycompany/holamundo/CicloFor.java
@@ -0,0 +1,14 @@
+package com.mycompany.holamundo;
+/**
+ *
+ * @author katyc
+ */
+public class CicloFor {
+    
+    public static void main(String args[]) {
+
+        for(var i= 0; i < 3; i ++){
+            System.out.println("i = " + i);
+        }
+    } 
+}
