f7a0848b6d5901e24fbf910181049efcb7f99b8f
Catalina Carreto <isccatalinacarreto27@gmail.com>
2019-10-18 15:21:17 -0500

    feat: Creacion de Clases

---
+++ creacionClasesObjetos/src/main/java/com/mycompany/creacionclasesobjetos/Persona.java
@@ -0,0 +1,18 @@
+package com.mycompany.creacionclasesobjetos;
+
+/**
+ *
+ * @author katyc
+ */
+public class Persona {
+
+    // Atributos
+    String nombre;
+    String apellido;
+
+    //Metodos
+    public void desplegarNombre() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
+}
