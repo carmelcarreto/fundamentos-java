1750cc9ed7c07f41d552cbd495812caf5da60e2a
Catalina Carreto <isccatalinacarreto27@gmail.com>
2019-11-05 19:10:07 -0600

    feat: Uso de Paquetes

---
+++ EjemploPaquetes/src/main/java/EjemploPaquetePrueba.java
@@ -0,0 +1,25 @@
+
+//import com.gm.Utileria;
+//import com.gm.*;
+
+import static com.gm.Utileria.imprimir;
+
+/**
+ *
+ * @author katyc
+ */
+public class EjemploPaquetePrueba {
+
+    public static void main(String[] args) {
+        //usando el nombre de la clase completamente calificado
+        //fully qualified name
+        //com.gm.Utileria.imprimir("Hola");
+
+        //haciendo un import de la clase
+        //Utileria.imprimir("Hola");
+        
+        //haciendo un import de tipo static
+        imprimir("Hola");
+    }
+
+}
