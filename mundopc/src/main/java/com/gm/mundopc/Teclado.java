d187a622cec83e27bff56a8611e9557e554beff3
Catalina Carreto <isccatalinacarreto27@gmail.com>
2019-12-09 12:09:50 -0600

    feat: Laboratorio final MundoPC

---
+++ mundopc/src/main/java/com/gm/mundopc/Teclado.java
@@ -0,0 +1,22 @@
+package com.gm.mundopc;
+
+/**
+ *
+ * @author katyc
+ */
+public class Teclado extends DispositivoEntrada {
+
+    private final int idTeclado;
+    private static int contadorTeclado;
+
+    //constructor que inicializa las variables
+    public Teclado(String tipoEntrada, String marca) {
+        super(tipoEntrada, marca);
+        idTeclado = ++contadorTeclado;
+    }
+
+    @Override
+    public String toString() {
+        return "Teclado{" + "idTeclado=" + idTeclado + ", " + super.toString();
+    }
+}
